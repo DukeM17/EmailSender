1166fd41ea78d0d3380d42176f71f5d7394e66c4
DukeM17 <57226668+DukeM17@users.noreply.github.com>
2024-06-11 21:52:19 +0100

    add email app

---
+++ EmailSender/EmailSender/EmailSenderService.cs
@@ -0,0 +1,55 @@
+﻿using MimeKit;
+
+namespace EmailSender;
+
+public interface IEmailSenderService
+{
+    MimeMessage CreateMessage(string fromEmail, string toEmail, string subject, string body);
+    bool SendEmail(MimeMessage message, string server = "smtp.gmail.com");
+}
+
+public class EmailSenderService : IEmailSenderService
+{
+    public EmailSenderService()
+    {
+    }
+
+    public bool SendEmail(MimeMessage message, string server = "smtp.gmail.com")
+    {
+        try
+        {
+            using (var client = new MailKit.Net.Smtp.SmtpClient())
+            {
+                client.Connect(server, 587, false);
+
+                // Note: only needed if the SMTP server requires authentication
+                client.Authenticate("m.duke401@gmail.com", "ubuf ezth nckq jcbw\r\n");
+
+                client.Send(message);
+                client.Disconnect(true);
+            }
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return false;
+        }
+    }
+
+    public MimeMessage CreateMessage(string fromEmail, string toEmail, string subject, string body)
+    {
+        var message = new MimeMessage();
+
+        message.From.Add(new MailboxAddress("Joey Tribbiani", fromEmail));
+        message.To.Add(new MailboxAddress("ToMailName", toEmail));
+        message.Subject = subject;
+        message.Body = new TextPart("plain")
+        {
+            Text = body
+        };
+
+        return message;
+    }
+}
---
+++ EmailSender/EmailSender/Program.cs
@@ -0,0 +1,24 @@
+﻿using EmailSender;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var host = Host.CreateDefaultBuilder()
+    .ConfigureServices((context, services) =>
+    {
+        //TODOM: add appsettings
+        services.AddTransient<IEmailSenderService, EmailSenderService>();
+    })
+    .Build();
+
+var emailService = host.Services.GetService<IEmailSenderService>();
+var message = emailService!.CreateMessage("m.duke401@gmail.com", "morin24792@noefa.com", "Test Email", "This is my test email to my temp email account");
+var result = emailService.SendEmail(message);
+
+if (result)
+{
+    Console.WriteLine("Email successfully sent");
+}
+else 
+{
+    Console.WriteLine("Email not sent");
+}
